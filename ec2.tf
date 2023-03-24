2cd0a0d5e78169b0d0a05dcf2d12aef6d4c2e333
sravani940 <126774624+sravani940@users.noreply.github.com>
2023-03-24 14:55:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAY6E6BTLNTJXBG6PB"
-    secret_key = "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"
+  access_key = "access key"
+    secret_key = "secret key"
 }
 resource "aws_instance" "ec2-terraform" {
   ami           =  "ami-0376ec8eacdf70aae"
